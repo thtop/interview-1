a0e8ac7eb870a3dd30b7bca0ce56d19f9b572e57
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-25 06:16:35 +0700

    Finished item 64. Implementing An Array

---
+++ 3-array/MyArray.js
@@ -0,0 +1,51 @@
+// Coding...
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length -1; i++) {
+            this.data[i] = this.data[i+1];
+        }
+        delete this.data[this.length-1]
+        this.length--;
+    }
+
+}
+
+const objArray = new MyArray();
+objArray.push('hi');
+objArray.push('you');
+objArray.push('!');
+// objArray.pop();
+// objArray.pop();
+console.log('DELETE: ', objArray.delete(0));
+objArray.push('are');
+objArray.push('nice');
+console.log('DELETE: ', objArray.delete(1));
+console.log(objArray);
