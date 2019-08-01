9176646485f19cc80b256e56207539035a79319f
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-02 05:38:47 +0700

    Added user.js file

---
+++ 4-hash-tables/user.js
@@ -0,0 +1,15 @@
+let user = {
+    age: 24,
+    name: 'Kylie',
+    magic: true,
+    scream: { function() {
+        console.log('ahhhhhhh!');
+    }}
+}
+
+age // O(1)
+user.spell = 'abra kadabra'; // O(1)
+user.scream(); // O(1)
+
+const a = new Map();
+// Sets
