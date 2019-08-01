fffddaf2349b65a06dfbd5325848c9bb77b0dcb5
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-02 05:32:53 +0700

    Finished item 74. Hash Collisions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Coding...
+let user = {
+    age: 24,
+    name: 'Kylie',
+    magic: true,
+    scream: { function() {
+        console.log('ahhhhhhh!');
+    }}
+}
+
+age // O(1)
+user.spell = 'abra kadabra'; // O(1)
+user.scream(); // O(1)
+
+const a = new Map();
+// Sets
